8e3eff6eeccc7dc413421b9894b8a199fdca721f
Bill <souvatzisbill@gmail.com>
2019-03-26 14:13:34 +0200

    removed placeholder tests

--- tests/unit/jobsList.spec.js
+++ tests/unit/jobsList.spec.js
@@ -1,8 +1,8 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/JobsList.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
+describe('JobsList.vue', () => {
+  it('contains', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
